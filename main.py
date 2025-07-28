caaa610cb28971ee3d9b789c7bc69952659f8d50
akanneman <kannemanadrian@gmail.com>
2025-07-28 15:55:47 -0700

    Add book reader script to load and print text files

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, encoding='utf-8') as f:
+        text = f.read()
+    return text
+
+def main():
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    print(text)
+
+if __name__ == "__main__":
+    main()
